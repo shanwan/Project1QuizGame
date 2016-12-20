4a61fbaa2c39d3418dab8cfb8204e27cb1e81f3d
ShanWan <shan.wan.loo@gmail.com>
2016-12-20 10:36:22 +0800

    setting up the project

---
+++ main.js
@@ -0,0 +1,22 @@
+numberOfQuestions()
+
+// It should return an integer that is the number of questions in a game
+currentQuestion()
+
+// It should return an integer that is the zero-based index of the current question in the quiz
+correctAnswer()
+
+// It should return an integer that is the zero-based index the correct answer for the current question
+numberOfAnswers()
+
+// It should return an integer that is the number of choices for the current question
+playTurn(choice)
+
+// It should take a single integer, which specifies which choice the current player wants to make. It should return a boolean true/false if the answer is correct.
+isGameOver()
+
+// It should return a true or false if the quiz is over.
+whoWon()
+
+// It should return 0 if the game is not yet finished. Else it should return either 1 or 2 depending on which player won. It should return 3 if the game is a draw.
+restart()
